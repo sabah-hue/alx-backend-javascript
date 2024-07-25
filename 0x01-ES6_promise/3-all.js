f974631697303ed7728928aba04d84a2882cbeb4
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-25 05:35:39 +0300

    output issue

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((res) => {
     console.log(res[0].body, res[1].firstName, res[1].lastName);
-  }).catch((e)=>console.log(e));
+  });
 };
