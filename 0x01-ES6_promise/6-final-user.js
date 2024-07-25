e3e28cab3ffa7b461b6f8d13dace0667785025eb
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-25 04:55:56 +0300

    allsettled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((res) => {
+      res.forEach((result) => (
+        {
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : result.reason,
+        }
+      ));
+    });
+}
