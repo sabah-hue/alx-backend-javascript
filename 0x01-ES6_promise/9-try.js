d83a6a6b6863052882c61a5e62fbca2e6e7c0f83
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-25 05:51:59 +0300

    queue

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,3 +1,11 @@
-export default function guardrail(mathFunction){
-    
-};
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+    queue.push('Guardrail was processed');
+  } catch (e) {
+    queue.push(String(e));
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
