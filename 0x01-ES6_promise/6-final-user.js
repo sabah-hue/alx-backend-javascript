6eb5c41ae66aee091478482d6b7a89f2f157415b
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-25 05:14:16 +0300

    faster

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((res) => {
-      res.forEach((result) => (
+      res.map((result) => (
         {
           status: result.status,
           value: result.status === 'fulfilled' ? result.value : String(result.reason),
