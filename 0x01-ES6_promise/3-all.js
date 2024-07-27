a09663b5d5221310727acae4df0e0017da1aa836
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-27 04:54:06 +0300

    remove return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then(([photoResult, userResult]) => {
+  Promise.all([uploadPhoto(), createUser()]).then(([photoResult, userResult]) => {
     console.log(photoResult.body, userResult.firstName, userResult.lastName);
   });
 }
