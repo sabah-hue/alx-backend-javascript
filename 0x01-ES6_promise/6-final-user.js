ae1165f082a02f545d8b1dbec67c96d79fad3e17
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-27 05:53:12 +0300

    debug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((res) => {
-      res.map((result) => (
-        {
-          status: result.status,
-          value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      const result = [];
+      res.forEach((element) => {
+        if (element.status === 'fulfilled') {
+          result.push({ status: element.status, value: element.value });
+        } else {
+          result.push({ status: element.status, value: `${element.reason}` });
         }
-      ));
+      });
+      return result;
     });
 }
