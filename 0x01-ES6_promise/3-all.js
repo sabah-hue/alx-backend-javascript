6ac6380411db893c75c8e497ebfb327dc3b77289
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-27 05:57:32 +0300

    debug

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const data = Promise.all([uploadPhoto(), createUser()]).then(([photoResult, userResult]) => {
-    console.log(photoResult.body, userResult.firstName, userResult.lastName);
+  const data = Promise.all([uploadPhoto(), createUser()]).then((res) => {
+    // console.log(`${photoResult}.body, ${userResult}.firstName, ${userResult}.lastName`);
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
   }).catch(() => console.log('Signup system offline'));
   return data;
 }
