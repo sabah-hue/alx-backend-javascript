fea9f978c1c2ef645f8b62408fea7ad80215fd84
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-31 05:56:43 +0300

    fix output

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const arr = students.filter((e) => e.location === city);
   return arr.map((e) => {
-    const student = newGrades.find((element) => element.id === e.id);
-    if (student) return { ...e, grade: student.grade };
-
+    const student = newGrades.find((element) => element.studentId === e.id);
+    if (student) {
+      if (student.grade) return { ...e, grade: student.grade };
+    }
     return { ...e, grade: 'N/A' };
   });
 }
