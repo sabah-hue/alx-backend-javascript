64bdb548db4a24eda63d9807bdfdb609d6273d4d
sabah-hue <sabah.abdelbaset@gmail.com>
2024-07-31 05:46:46 +0300

    combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,5 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const arr = students.filter((e) => e.location === city);
-  console.log(arr);
   return arr.map((e) => {
     const student = newGrades.find((element) => element.id === e.id);
     if (student) return { ...e, grade: student.grade };
