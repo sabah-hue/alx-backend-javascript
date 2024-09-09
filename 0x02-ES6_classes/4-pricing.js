932415e8cc9ca39d0bb7acc87cb6527b4f76af21
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-09 08:16:24 +0300

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,8 +1,38 @@
-import Currency from './3-currency'
+import Currency from './3-currency';
 
-export default class Pricing extends Currency(){
-    constructor(amount, currency) {
-      this.amount = amount;
-      this.currency = currency;
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
     }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
