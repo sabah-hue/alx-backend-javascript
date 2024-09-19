dab452f7b8fc8a55b32f6c7ccf5b48e735696409
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-19 06:41:45 +0300

    http edit

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,12 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
+const serverRequest = (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  
   res.end('Hello Holberton School!');
-});
+};
 
-app.listen(1245, () => {
-  console.log('Server running ....');
-});
+const app = http.createServer(serverRequest);
+
+app.listen(1245);
 
 module.exports = app;
