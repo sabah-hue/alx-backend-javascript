f54c0902cde7d63259e14c3d9c791b801ca10e12
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-21 20:17:47 +0300

    same error

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,34 +3,30 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
-// new endpoint
+
 app.get('/cart/:id(\\d+)', (req, res) => {
   const id = req.params.id;
 
   res.send(`Payment methods for cart ${id}`);
 });
-// new endpoint
-app.get('/available_payments', (request, response) => {
-  response.set("Content-Type", "application/json");
-  const payMethods = {
-payment_methods: {
-        credit_cards: true,
-        paypal: false
-}
-  }
-  response.send(payMethods);
+
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
-app.post('/login', (request, response) => {
-  const userName = request.body.userName;
-  if (userName) {
-response.send(`Welcome ${userName}`);
-  } else {
-response.status(404).send();
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
   }
+
+  res.send(`Welcome ${username}`);
 });
 
 app.listen(PORT, () => {
