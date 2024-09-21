c9c8dc48a9c25bf6fae6131e1a943f1a17708a19
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-21 05:38:50 +0300

    promise

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const chai = require("chai");
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('when success == true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      });
+  });
+});
