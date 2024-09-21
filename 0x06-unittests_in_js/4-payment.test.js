d8e54594194f08dbe8def35de11c32e0c6d9a300
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-21 05:35:43 +0300

    debug

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -2,24 +2,24 @@ const chai = require("chai");
 const expect = chai.expect;
 // import sinon
 const sinon = require("sinon");
+const Utils = require('./utils');
 
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./4-payment.js');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi by Utils function', () => {
-    const x = sinon.spy(Utils);
+    const x = sinon.spy(console);
     const y = sinon.stub(Utils, 'calculateNumber');
+
     y.returns(10);
     sendPaymentRequestToApi(100, 20);
 
     expect(y.calledWith('SUM', 100, 20)).to.be.true;
     expect(y.callCount).to.be.equal(1);
 
-    expect(x.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(x.calculateNumber.callCount).to.be.equal(1);
-
+    expect(x.log.calledWith('The total is: 10')).to.be.true;
+    expect(x.log.callCount).to.be.equal(1);
     y.restore();
-    x.calculateNumber.restore();
+    x.log.restore();
   });
 });
